2d84db1dbed0a609a7bd602b2c260b9640234ea3
alessandrolongo99 <longoalessandro153@gmail.com>
2022-09-07 22:55:21 +0200

    header

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,50 @@
+<header>
+    <div class="top-bar">
+        <div class="container text-end text-white">
+            <a href="#" class="pe-5">DC POWER&#8480; VISA&reg;</a>
+            <a href="#">ADDITIONAL DC SITES&#9660;</a>
+        </div>
+    </div>
+
+    <nav class="nav-bar">
+        <div class="container align-items-center d-flex justify-content-between">
+            <a class="navbar-brand" href="#">
+                <img src="{{URL('assets/images/dc-logo.png')}}" alt="dc-logo">
+            </a>
+            <div class="d-flex align-items-center">
+                <ul class="d-flex">
+                    <li>
+                        <a href="#">CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a href="#">COMICS</a>
+                    </li>
+                    <li>
+                        <a href="#">MOVIES</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">GAMES</a>
+                    </li>
+                    <li>
+                        <a href="#">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a href="#">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href="#">FANS</a>
+                    </li>
+                    <li>
+                        <a href="#">NEWS</a>
+                    </li>
+                    <li>
+                        <a href="#">SHOP</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
